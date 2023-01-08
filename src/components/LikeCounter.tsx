28110d1c0a46dff8bee8bbb0282fbc855effe381
unknown <viveka.barneaud@gmail.com>
2023-01-08 16:21:00 +0100

    like counter

--- src/components/LikeCounter.tsx
+++ src/components/LikeCounter.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "solid-js";
+import { JSX, createSignal } from "solid-js";
 
 type LikeCounterProps = {
     likes: number;
@@ -6,10 +6,23 @@ type LikeCounterProps = {
 }
 
 function LikeCounter(props: LikeCounterProps): JSX.Element {
+    const [likes, setLikes] = createSignal<number>(props.likes);
+    const [userInput, setUserInput] = createSignal<number>(0);
+
+    const like = (): void => {
+        setLikes(likes() + 1)
+        setUserInput(userInput() + 1);
+    }
+
+    const dislike = (): void => {
+        setLikes(likes() - 1);
+        setUserInput(userInput() - 1);
+    }
+
     return (<div class="flex justify-center items-center p-2 gap-x-2 rounded-lg bg-[#F1F4F9]">
-        <button class="appearence-none text-sm text-[#C5C3E7] font-bold">+</button>
-        <span class="text-xs text-[#5F62A1] font-bold">{props.likes}</span>
-        <button class="appearence-none text-sm text-[#C5C3E7] font-bold">-</button>
+        <button class="appearence-none text-sm text-[#C5C3E7] font-bold" disabled={userInput() >= 1} onClick={() => like()}>+</button>
+        <span class="text-xs text-[#5F62A1] font-bold">{likes()}</span>
+        <button class="appearence-none text-sm text-[#C5C3E7] font-bold" disabled={userInput() <= -1} onClick={() => dislike()}>-</button>
     </div>)
 }
 
